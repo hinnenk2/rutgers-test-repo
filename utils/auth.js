baaa5e9fe8b180bfe5d61ddf5c7fab091e70a7b9
Ian Hinnenkamp <ianhinnenkamp@Ians-Air.fios-router.home>
2023-08-02 19:59:57 -0400

    removed

--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-const withAuth = (req, res, next) => {    //redirects user to login page if not currently logged in, required to start the app
-    if (!req.session.user_id) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
